f969368d0b3ca295490e26db9d48a8da18ca8638
prooheckcp <vasco.soares.2001@gmail.com>
2020-05-22 18:17:27 +0100

    Setting up the files

---
+++ config.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.title = "ToDo App"         -- The window title (string)
+    t.console = true
+    t.window.borderless = true
+    t.window.resizable = true
+    t.window.width = 800
+    t.window.height = 600
+
+    t.window.icon = nil  
+
+end
